c5bd8853868b2bd0257e9be665008696233ed68d
snezk456 <86143353+snezk456@users.noreply.github.com>
2022-11-12 14:33:16 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-function preload() {
+lipstickX=0;
+lipstickY=0;
 
+function preload() {
+    lipstick = loadImage('https://i.postimg.cc/05zLZnrq/lipstick.png');
 }
 
 function setup() {
@@ -22,6 +25,8 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
+        lipstickX = results[0].pose.nose.x;
+        lipstickY = results[0].pose.nose.y;
         console.log("lipstick x = " + results[0].pose.lipstick.x);
         console.log("lipstick y = " + results[0].pose.lipstick.y);
     }
@@ -29,6 +34,7 @@ function gotPoses(results)
 
 function draw() {
     image(video, 0, 0, 300, 300);
+    image(lipstick, lipstickX,lipstickY, 40, 40);
 }
 
 function take_snapshot() {
